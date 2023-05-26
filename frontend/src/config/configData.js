84974a7653c400c95207699b1de216c60b10694d
Andrea De Lorenzis <andreadelorenzis99@gmail.com>
2023-05-26 12:44:42 +0200

    Update configData.js

--- frontend/src/config/configData.js
+++ frontend/src/config/configData.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
     apiKey: "",
-    authDomain: "blipquiz.firebaseapp.com",
-    projectId: "blipquiz",
-    storageBucket: "blipquiz.appspot.com",
-    messagingSenderId: "747854309345",
-    appId: "1:747854309345:web:dffec72a2efa5d1df454a0"
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
